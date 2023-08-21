19c6fa427896402693f98109f14b7852d1bda6e8
timrcm <tim@timrcm.com>
2023-08-21 16:15:59 -0600

    more refactoring, check for tie condition

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+from player import Player
+
+
 class Game:
     def __init__(self):
         self.grid_coords = {
@@ -5,6 +8,13 @@ class Game:
             4: '4', 5: '5', 6: '6',
             7: '7', 8: '8', 9: '9'
         }
+        self.winning_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+        self.turn_counter = 9
+
+    def check_for_winner(self):
+        if self.turn_counter == 0:
+            print('The game is tied. Better luck next time!')
+            exit(0)
 
     def draw_board(self):
         print('\n')
@@ -16,3 +26,26 @@ class Game:
                 print('\n-----------')
             else:
                 print(' |', end='')
+
+    def place_piece(self, player: Player):
+        self.draw_board()
+        # This is ugly. What is a better selection method?
+        try:
+            location = int(input(f"Hi, {player.name}. Where would you like to place your piece?\n"
+                                 f"Enter a space number that is not yet taken: \n"))
+        except ValueError:
+            print('\nPlease enter a valid integer.')
+            self.place_piece(player)
+        else:
+            if location in self.grid_coords.keys():
+                if self.grid_coords[location] != 'X' and self.grid_coords[location] != 'Y':
+                    self.grid_coords[location] = f'{player.piece}'
+                    self.turn_counter -= 1
+                    self.check_for_winner()
+                    print(f'{self.turn_counter} turns left!')
+                else:
+                    print('\nThat space is already taken. Try again.')
+                    self.place_piece(player)
+            else:
+                print('\nSorry, that coordinate was not found. Please try again.')
+                self.place_piece(player)
--- main.py
+++ main.py
@@ -14,37 +14,12 @@ def select_players():
     return player_1, player_2
 
 
-def check_for_winner():
-    pass
-
-
-def place_piece(player):
-    game.draw_board()
-    # This is ugly. What is a better selection method?
-    try:
-        location = int(input(f"Hi, {player.name}. Where would you like to place your piece?\n"
-                             f"Enter a space number that is not yet taken: \n"))
-    except ValueError:
-        print('\nPlease enter a valid integer.')
-        place_piece(player)
-    else:
-        if location in game.grid_coords.keys():
-            if game.grid_coords[location] != 'X' and game.grid_coords[location] != 'Y':
-                game.grid_coords[location] = f'{player.piece}'
-            else:
-                print('\nThat space is already taken. Try again.')
-                place_piece(player)
-        else:
-            print('\nSorry, that coordinate was not found. Please try again.')
-            place_piece(player)
-
-
 if __name__ == '__main__':
     players = select_players()
     active_player = players[0]
     while True:
         # Keep swapping turns and playing until the game ends
-        place_piece(active_player)
+        game.place_piece(active_player)
         if active_player == players[0]:
             active_player = players[1]
         else:
